d6691bf76ddcc1a3b6988262cfbf0300bb5100b8
ioretcio <goncharukyou@gmail.com>
2023-03-26 17:21:39 +0300

    Add project files.

---
+++ queueing theory/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace queueing_theory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.відкритиМатричнеПоданняЗадачіToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.зберегтиЗадачуВФайлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.task1grid = new System.Windows.Forms.DataGridView();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.task1grid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1094, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.відкритиМатричнеПоданняЗадачіToolStripMenuItem,
+            this.зберегтиЗадачуВФайлToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(143, 20);
+            this.файлToolStripMenuItem.Text = "Лабораторна робота 1";
+            // 
+            // відкритиМатричнеПоданняЗадачіToolStripMenuItem
+            // 
+            this.відкритиМатричнеПоданняЗадачіToolStripMenuItem.Name = "відкритиМатричнеПоданняЗадачіToolStripMenuItem";
+            this.відкритиМатричнеПоданняЗадачіToolStripMenuItem.Size = new System.Drawing.Size(264, 22);
+            this.відкритиМатричнеПоданняЗадачіToolStripMenuItem.Text = "Відкрити матричне подання задачі";
+            this.відкритиМатричнеПоданняЗадачіToolStripMenuItem.Click += new System.EventHandler(this.відкритиМатричнеПоданняЗадачіToolStripMenuItem_Click);
+            // 
+            // зберегтиЗадачуВФайлToolStripMenuItem
+            // 
+            this.зберегтиЗадачуВФайлToolStripMenuItem.Name = "зберегтиЗадачуВФайлToolStripMenuItem";
+            this.зберегтиЗадачуВФайлToolStripMenuItem.Size = new System.Drawing.Size(264, 22);
+            this.зберегтиЗадачуВФайлToolStripMenuItem.Text = "Зберегти задачу в файл";
+            this.зберегтиЗадачуВФайлToolStripMenuItem.Click += new System.EventHandler(this.зберегтиЗадачуВФайлToolStripMenuItem_Click);
+            // 
+            // task1grid
+            // 
+            this.task1grid.AllowUserToAddRows = false;
+            this.task1grid.AllowUserToResizeColumns = false;
+            this.task1grid.AllowUserToResizeRows = false;
+            this.task1grid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.task1grid.BackgroundColor = System.Drawing.Color.White;
+            this.task1grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.task1grid.ColumnHeadersVisible = false;
+            this.task1grid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.task1grid.GridColor = System.Drawing.Color.Black;
+            this.task1grid.Location = new System.Drawing.Point(2, 2);
+            this.task1grid.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.task1grid.Name = "task1grid";
+            this.task1grid.RowHeadersVisible = false;
+            this.task1grid.RowHeadersWidth = 51;
+            this.task1grid.RowTemplate.DefaultCellStyle.BackColor = System.Drawing.Color.White;
+            this.task1grid.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.task1grid.RowTemplate.DefaultCellStyle.ForeColor = System.Drawing.Color.Black;
+            this.task1grid.RowTemplate.Height = 24;
+            this.task1grid.Size = new System.Drawing.Size(537, 524);
+            this.task1grid.TabIndex = 1;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.BackColor = System.Drawing.Color.White;
+            this.numericUpDown1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDown1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.numericUpDown1.Location = new System.Drawing.Point(2, 219);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(533, 21);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(2, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(533, 217);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Користувацький розмір";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(2, 344);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(533, 26);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Кількість кроків";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.BackColor = System.Drawing.Color.White;
+            this.numericUpDown2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.numericUpDown2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDown2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.numericUpDown2.Location = new System.Drawing.Point(2, 372);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(533, 21);
+            this.numericUpDown2.TabIndex = 4;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(2, 403);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(533, 51);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Обрахувати";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.White;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.richTextBox1.Location = new System.Drawing.Point(2, 458);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(533, 64);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 24);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1094, 558);
+            this.tabControl1.TabIndex = 8;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.tableLayoutPanel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Size = new System.Drawing.Size(1086, 532);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.task1grid, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(2, 2);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1082, 528);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.richTextBox1, 0, 5);
+            this.tableLayoutPanel2.Controls.Add(this.numericUpDown1, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.button1, 0, 4);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.numericUpDown2, 0, 3);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(543, 2);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 6;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 63.15789F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 36.84211F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 31F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 55F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 67F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(537, 524);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Size = new System.Drawing.Size(1086, 531);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1094, 582);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "queueing theory";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.task1grid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.DataGridView task1grid;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripMenuItem відкритиМатричнеПоданняЗадачіToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem зберегтиЗадачуВФайлToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
---
+++ queueing theory/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace queueing_theory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            task1grid.ColumnCount = Convert.ToInt32(numericUpDown1.Value);
+            task1grid.RowCount = Convert.ToInt32(numericUpDown1.Value);
+        }
+        private void зберегтиЗадачуВФайлToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string fileContent = getGridText(task1grid);
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(DateTime.Now.ToString().Replace(':', ' ') + ".txt", false, System.Text.Encoding.Default))
+                {
+                    sw.Write(fileContent);
+                }
+            }
+            catch (Exception exe)
+            {
+                MessageBox.Show(exe.Message);
+            }
+        }
+        string getGridText(DataGridView dataGrid)
+        {
+            string result = "";
+            for (int i = 0; i < dataGrid.RowCount; i++)
+            {
+                for (int j = 0; j < dataGrid.ColumnCount; j++)
+                {
+                    result += Convert.ToDouble(dataGrid[j, i].Value) + " ";
+                }
+                result += "\n";
+            }
+            return result;
+        }
+        void putDataToGrid(string data, DataGridView dataGrid)
+        {
+            string[] rows = data.Split('\n');
+            dataGrid.RowCount = rows.Length - 1;
+            bool f = true;
+            for (int i = 0; i < dataGrid.RowCount; i++)
+            {
+                string[] columns = rows[i].Split(' ');
+                if (f)
+                {
+                    dataGrid.ColumnCount = columns.Length - 1; f = false;
+                }
+                for (int j = 0; j < dataGrid.ColumnCount; j++)
+                {
+                    dataGrid[j, i].Value = columns[j];
+                }
+            }
+        }
+        private void відкритиМатричнеПоданняЗадачіToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                var fileStream = openFileDialog.OpenFile();
+                string alldata = "";
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    alldata = reader.ReadToEnd();
+                    putDataToGrid(alldata, task1grid);
+                }
+            }
+            numericUpDown1.Value = task1grid.RowCount;
+            numericUpDown2.Value = task1grid.RowCount;
+        }
+        bool checkGrid()
+        {
+            bool message=false;
+            for (int i = 0; i < task1grid.RowCount; i++)
+            {
+                double summ = 0;
+                for (int j = 0; j < task1grid.ColumnCount; j++)
+                {
+                    summ += Convert.ToDouble(task1grid[j, i].Value.ToString().Replace('.', ','));
+                }
+                if (Math.Abs(summ - 1) > 0.000000001)
+                {
+                    for (int j = 0; j < task1grid.ColumnCount; j++)
+                    {
+                        task1grid.Rows[i].Cells[j].Style.BackColor = System.Drawing.Color.Pink;
+                    }
+                    message = true;
+                }
+                else
+                {
+                    for (int j = 0; j < task1grid.ColumnCount; j++)
+                    {
+                        task1grid.Rows[i].Cells[j].Style.BackColor = System.Drawing.Color.White;
+                    }
+                }
+            }
+            if(message)
+            MessageBox.Show("В матриці присутні не одиничні рядки");
+            return !message;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkGrid())
+            {
+                List<double[]> answers = new List<double[]>();
+                int k = (int)numericUpDown2.Value;
+                int dimension = (int)task1grid.RowCount;
+                 
+                double[,] matrix = new double[dimension, dimension];
+                for (int i = 0; i < dimension; i++)
+                {
+                    for (int j = 0; j < dimension; j++)
+                    {
+                        matrix[j, i] = Convert.ToDouble(task1grid[i, j].Value);
+                    }
+                }
+                answers.Add(new double[dimension]); // після першого кроку
+                for (int i = 0; i < dimension; i++)
+                {
+                    answers[0][i] = matrix[0, i];
+                }
+                for (int i = 1; i < k; i++)
+                {
+                    answers.Add(new double[dimension]);
+                    for (int j = 0; j < dimension; j++)
+                    {
+                        for (int l = 0; l < dimension; l++)
+                        {
+                            answers[i][j] += answers[i - 1][l] * matrix[l, j];
+                        }
+                        answers[i][j] = Math.Round(answers[i][j], 3);
+                    }
+                }
+                richTextBox1.Text = "";
+                for (int i = 0; i < k; i++)
+                {
+                    richTextBox1.Text += "Ймовірності після кроку " + (i + 1).ToString() + ":\n";
+                    for (int z = 0; z < dimension; z++)
+                    {
+                        richTextBox1.Text += answers[i][z].ToString() + "    ";
+                    }
+                    richTextBox1.Text += "\n";
+                }
+            }
+        }
+    }
+}
